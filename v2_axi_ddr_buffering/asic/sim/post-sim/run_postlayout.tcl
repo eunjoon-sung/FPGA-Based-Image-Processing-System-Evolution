d59c509eeb098c0a55a06cb8a532f9dc2d3fe8a5
Sung Eunjoon <1225seong@gmail.com>
2026-03-02 18:17:41 +0900

    Create run_postlayout.tcl

---
+++ v2_axi_ddr_buffering/asic/sim/post-sim/run_postlayout.tcl
@@ -0,0 +1,10 @@
+# run_postlayout.tcl
+
+# netlist file
+/home/s_2025_h007/PROJECT/HDMI_DDR/pnr/outputs/AXI4_writer_final.v
+
+# tb file
+/home/s_2025_h007/PROJECT/HDMI_DDR/sim/post-sim/tb_axi4_writer_pls.v
+
+#library file
+/home/s_2025_h007/PROJECT/HDMI_DDR/edu_lib/verilog/slow_vdd1v0_basicCells.v
