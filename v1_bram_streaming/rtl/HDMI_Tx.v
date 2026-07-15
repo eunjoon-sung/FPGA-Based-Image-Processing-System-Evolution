3f3c6ec06ff598c16425c2d22af8495388ced129
Sung Eunjoon <1225seong@gmail.com>
2026-07-15 15:20:36 +0900

    HDMI_Tx.v 업데이트

--- v1_bram_streaming/rtl/HDMI_Tx.v
+++ v1_bram_streaming/rtl/HDMI_Tx.v
@@ -1,31 +1,27 @@
 module HDMI_Tx(
-    input PXLCLK_I,       // 25MHz (Pixel Clock)
-    input PXLCLK_5X_I,    // 125MHz (Serial Clock)
-    input LOCKED_I,       // Clock Locked
-    input RST_I,          // Reset
+    input PXLCLK_I,       // 25MHz
+    input PXLCLK_5X_I,    // 125MHz
+    input LOCKED_I,
+    input RST_I,
+    input VGA_HS_I,
+    input VGA_VS_I,
+    input VGA_DE_I,
+    input [23:0] VGA_RGB_I, // {R, G, B}
 
-    input VGA_HS_I,       // HSYNC
-    input VGA_VS_I,       // VSYNC
-    input VGA_DE_I,       // Data Enable
-    input [23:0] VGA_RGB_I, // {R, G, B} 8bit씩
-
-    output HDMI_CLK_P,    // HDMI Clock (+)
-    output HDMI_CLK_N,    // HDMI Clock (-)
-    output [2:0] HDMI_DATA_P, // HDMI Data (+) R,G,B
-    output [2:0] HDMI_DATA_N  // HDMI Data (-)
+    output HDMI_CLK_P, output HDMI_CLK_N,
+    output [2:0] HDMI_DATA_P, output [2:0] HDMI_DATA_N
 );
 
     wire [9:0] TMDS_red, TMDS_green, TMDS_blue;
     
-    // 1. 8b/10b 인코딩 (아까 만든 파일 사용)
+    // 1. 8b/10b 인코딩 (기존과 동일)
     TMDS_encoder encode_R(.clk(PXLCLK_I), .VD(VGA_RGB_I[23:16]), .CD(2'b00), .VDE(VGA_DE_I), .TMDS(TMDS_red));
     TMDS_encoder encode_G(.clk(PXLCLK_I), .VD(VGA_RGB_I[15:8]),  .CD(2'b00), .VDE(VGA_DE_I), .TMDS(TMDS_green));
     TMDS_encoder encode_B(.clk(PXLCLK_I), .VD(VGA_RGB_I[7:0]),   .CD({VGA_VS_I, VGA_HS_I}), .VDE(VGA_DE_I), .TMDS(TMDS_blue));
 
-    // 2. 직렬화 및 출력 버퍼 (OSERDESE2 + OBUFDS)
-    // Zynq/Artix-7 전용 Primitive 사용
+    // 2. 5단계 시프터 및 ODDR (수정된 부분!)
     reg [4:0] TMDS_mod5 = 0;
-    reg [9:0] TMDS_shift_red = 0, TMDS_shift_green = 0, TMDS_shift_blue = 0, TMDS_shift_clk = 0;
+    reg [9:0] shift_red = 0, shift_green = 0, shift_blue = 0, shift_clk = 0;
     
     always @(posedge PXLCLK_5X_I) begin
         if (RST_I) TMDS_mod5 <= 0;
@@ -36,22 +32,40 @@ module HDMI_Tx(
 
     always @(posedge PXLCLK_5X_I) begin
         if (shift_load) begin
-            TMDS_shift_red   <= TMDS_red;
-            TMDS_shift_green <= TMDS_green;
-            TMDS_shift_blue  <= TMDS_blue;
-            TMDS_shift_clk   <= 10'b1111100000; // Clock Pattern
+            shift_red   <= TMDS_red;
+            shift_green <= TMDS_green;
+            shift_blue  <= TMDS_blue;
+            shift_clk   <= 10'b1111100000;
         end else begin
-            TMDS_shift_red   <= TMDS_shift_red   >> 1;
-            TMDS_shift_green <= TMDS_shift_green >> 1;
-            TMDS_shift_blue  <= TMDS_shift_blue  >> 1;
-            TMDS_shift_clk   <= TMDS_shift_clk   >> 1;
+            // 2비트씩 밀어냄 (ODDR이 2개씩 가져가니까)
+            shift_red   <= shift_red   >> 2;
+            shift_green <= shift_green >> 2;
+            shift_blue  <= shift_blue  >> 2;
+            shift_clk   <= shift_clk   >> 2;
         end
     end
 
-    // 차동 신호 출력 (OBUFDS)
-    OBUFDS OBUFDS_red  (.I(TMDS_shift_red[0]),   .O(HDMI_DATA_P[2]), .OB(HDMI_DATA_N[2]));
-    OBUFDS OBUFDS_green(.I(TMDS_shift_green[0]), .O(HDMI_DATA_P[1]), .OB(HDMI_DATA_N[1]));
-    OBUFDS OBUFDS_blue (.I(TMDS_shift_blue[0]),  .O(HDMI_DATA_P[0]), .OB(HDMI_DATA_N[0]));
-    OBUFDS OBUFDS_clk  (.I(TMDS_shift_clk[0]),   .O(HDMI_CLK_P),     .OB(HDMI_CLK_N));
+    // 3. ODDR을 이용한 고속 출력 (DDR Mode)
+    // 125MHz 클럭 하나에 비트 2개(0번 비트, 1번 비트)를 실어 보냄 -> 250Mbps 달성
+    wire [2:0] ddr_data;
+    wire ddr_clk;
+
+    ODDR #( .DDR_CLK_EDGE("SAME_EDGE"), .INIT(1'b0), .SRTYPE("ASYNC") ) 
+    ODDR_r (.Q(ddr_data[2]), .C(PXLCLK_5X_I), .CE(1'b1), .D1(shift_red[0]),   .D2(shift_red[1]),   .R(RST_I), .S(1'b0));
+    
+    ODDR #( .DDR_CLK_EDGE("SAME_EDGE"), .INIT(1'b0), .SRTYPE("ASYNC") ) 
+    ODDR_g (.Q(ddr_data[1]), .C(PXLCLK_5X_I), .CE(1'b1), .D1(shift_green[0]), .D2(shift_green[1]), .R(RST_I), .S(1'b0));
+    
+    ODDR #( .DDR_CLK_EDGE("SAME_EDGE"), .INIT(1'b0), .SRTYPE("ASYNC") ) 
+    ODDR_b (.Q(ddr_data[0]), .C(PXLCLK_5X_I), .CE(1'b1), .D1(shift_blue[0]),  .D2(shift_blue[1]),  .R(RST_I), .S(1'b0));
+    
+    ODDR #( .DDR_CLK_EDGE("SAME_EDGE"), .INIT(1'b0), .SRTYPE("ASYNC") ) 
+    ODDR_c (.Q(ddr_clk),     .C(PXLCLK_5X_I), .CE(1'b1), .D1(shift_clk[0]),   .D2(shift_clk[1]),   .R(RST_I), .S(1'b0));
+
+    // 4. 차동 신호 버퍼 (OBUFDS)
+    OBUFDS OBUFDS_red  (.I(ddr_data[2]), .O(HDMI_DATA_P[2]), .OB(HDMI_DATA_N[2]));
+    OBUFDS OBUFDS_green(.I(ddr_data[1]), .O(HDMI_DATA_P[1]), .OB(HDMI_DATA_N[1]));
+    OBUFDS OBUFDS_blue (.I(ddr_data[0]), .O(HDMI_DATA_P[0]), .OB(HDMI_DATA_N[0]));
+    OBUFDS OBUFDS_clk  (.I(ddr_clk),     .O(HDMI_CLK_P),     .OB(HDMI_CLK_N));
 
 endmodule
